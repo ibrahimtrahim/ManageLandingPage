7d1367cb80e3090f4ec9d97283f40eeb58c12b52
ibrahimtrahim <trahimbrahim@gmail.com>
2023-10-21 14:48:42 +0000

    complete

--- app/js/script.js
+++ app/js/script.js
@@ -3,6 +3,11 @@ const body = document.querySelector('body');
 let overlay = document.getElementById("overlay");
 let menu = document.getElementById("menu");
 let toggle = document.getElementById("toggle");
+const carousel = document.querySelector('.testimonials__cards');
+const dots = document.querySelectorAll('.dot');
+
+let isDragging = false;
+let startX, startScrollLeft;
 
 overlay.style.opacity = "0";
 overlay.style.visibility = "hidden";
@@ -24,3 +29,38 @@ menubtn.onclick = function(){
         menu.src = "../images/icon-hamburger.svg";
     }
 }
+const dragStart = (e) => {
+    isDragging = true;
+    carousel.classList.add("dragging");
+    startX = e.type === "touchstart" ? e.touches[0].clientX : e.clientX;
+    startScrollLeft = carousel.scrollLeft;
+}
+const dragging = (e) => {
+    if(!isDragging) return;
+    const x = e.type === "touchmove" ? e.touches[0].clientX : e.clientX;
+    const walk = x - startX;
+    carousel.scrollLeft = startScrollLeft - walk;
+}
+
+const dragStop = () => {
+    isDragging = false;
+    carousel.classList.remove("dragging");
+}
+
+const navigateSlider = (index) => {
+    carousel.scrollLeft = index * carousel.clientWidth;
+}
+
+carousel.addEventListener("mousedown", dragStart);
+carousel.addEventListener("mousemove", dragging);
+document.addEventListener("mouseup", dragStop);
+
+carousel.addEventListener("touchstart", dragStart);
+carousel.addEventListener("touchmove", dragging);
+document.addEventListener("touchend", dragStop);
+
+dots.forEach((dot, index) => {
+    dot.addEventListener("click", () => {
+      navigateSlider(index);
+    });
+});
